1e7163c9e8bf236cc24c6b4e19104f539136471c
domjrivera <48341477+domjrivera@users.noreply.github.com>
2022-12-21 12:41:41 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -16,6 +16,18 @@ module.exports = function (eleventyConfig) {
       "dd LLL yyyy"
     );
   });
+  
+  
+  // Tags
+    eleventyConfig.addCollection('tagList', collection => {
+        const tagsSet = new Set();
+        collection.getAll().forEach(item => {
+            if (!item.data.tags) return;
+            item.data.tags.filter(tag => !['posts', 'all'].includes(tag)).forEach(tag => tagsSet.add(tag));
+        });
+        return Array.from(tagsSet).sort();
+    });
+
 
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
